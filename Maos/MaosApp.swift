4763afa7c8f6c2b4d78fde3f2bec97aed9b89623
ffadilaputra <i.fadilaputra@gmail.com>
2026-07-17 14:24:57 +0700

    Initial Commit

---
+++ Maos/MaosApp.swift
@@ -0,0 +1,32 @@
+//
+//  MaosApp.swift
+//  Maos
+//
+//  Created by Ivan on 17/07/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct MaosApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
